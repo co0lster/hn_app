1236a88c70c7a6e33150a67c5f7db430aaceabeb
co0lster <202254@edu.p.lodz.pl>
2019-02-10 01:23:09 +0100

    Articles class and mock data added

---
+++ lib/src/articles.dart
@@ -0,0 +1,55 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+
+const Article(
+  {this.text,
+  this.domain,
+  this.by,
+  this.age,
+  this.score,
+  this.commentsCount}
+    );
+}
+
+final articles = [
+  new Article(
+    text:
+      "Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),new Article(
+    text:
+      "Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),new Article(
+    text:
+      "Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),new Article(
+    text:
+      "Circular Shock Acoustic Waves in Ionosphere",
+    domain: "wiley.com",
+    by: "zdw",
+    age: "3 hours",
+    score: 177,
+    commentsCount: 62,
+  ),
+
+];
